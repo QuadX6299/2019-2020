ac6e959e3fe0859e54fe3a2ec801e8051149c96f
dkasabovn <dkasabovn@gmail.com>
2019-11-22 18:11:31 -0600

    added outreach tele to opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/OutreachTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/OutreachTele.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.OpMode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp(name = "Outreach", group = "TeleOp")
 public class OutreachTele extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
