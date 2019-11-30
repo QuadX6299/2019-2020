23628036539165d04a200688ed30dfa2a442f280
dkasabovn <dkasabovn@gmail.com>
2019-11-30 02:03:13 -0600

    finished all Nclasses & added them to NROBOT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/NGrabber.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/NGrabber.kt
@@ -3,21 +3,102 @@ package org.firstinspires.ftc.teamcode.Robot
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import org.openftc.revextensions2.ExpansionHubServo
 
-class NGrabber constructor(Op : OpMode) {
+class NGrabber constructor(Op : OpMode, moveOnInit : Boolean = true) {
     val Grabber : ExpansionHubServo = Op.hardwareMap.get(ExpansionHubServo::class.java, "grabber")
     val RotateGrabber : ExpansionHubServo = Op.hardwareMap.get(ExpansionHubServo::class.java, "rotateGrabber")
     val RotateAssembly : ExpansionHubServo = Op.hardwareMap.get(ExpansionHubServo::class.java, "rotateAssembly")
     val Horn : ExpansionHubServo = Op.hardwareMap.get(ExpansionHubServo::class.java, "push")
 
-    var offset2 = ORIENTATIONS.VERTICAl
+    init {
+        if (moveOnInit) {
+            resetPositions()
+        }
+    }
 
-    enum class ORIENTATIONS(val offset : Double){
+    var offset = ORIENTATIONS.VERTICAL
+    var hornDown = false
+    var grabberDown = false
+
+    enum class ORIENTATIONS(val difference : Double) {
         HORIZONTAL(.77),
-        VERTICAl(.45),
+        VERTICAL(.45),
         COLLECTION(-.235),
         CLAMP(.23),
         TRANSITION(.25)
     }
 
+    enum class POSITIONS {
+        COLLECTION,
+        TRANSITION,
+        HORIZONTALDEPO,
+        HORNDOWN,
+        HORNUP,
+        VERTICALDEPO,
+        CLAMPDOWN,
+        DROP
+    }
 
+    fun setAssemblyPosition(pose : POSITIONS) {
+        when (pose) {
+            POSITIONS.COLLECTION -> {
+                offset = ORIENTATIONS.COLLECTION
+                setManipulatorPosition(.71)
+            }
+            POSITIONS.TRANSITION -> {
+                setManipulatorPosition(.25)
+            }
+            POSITIONS.VERTICALDEPO -> {
+                offset = ORIENTATIONS.VERTICAL
+                setManipulatorPosition(.2)
+            }
+            POSITIONS.HORIZONTALDEPO -> {
+                offset = ORIENTATIONS.HORIZONTAL
+                setManipulatorPosition(.2)
+            }
+            /* Clamp Position Switch */
+            POSITIONS.CLAMPDOWN -> {
+                Grabber.position = .23
+            }
+            POSITIONS.DROP -> {
+                Grabber.position = 0.0
+            }
+            /* Horn Position Switch */
+            POSITIONS.HORNDOWN -> {
+                Horn.position = 0.0
+            }
+            POSITIONS.HORNUP -> {
+                Horn.position = 1.0
+            }
+        }
+    }
+
+    fun setManipulatorPosition(pos: Double){
+        RotateAssembly.position = pos
+        RotateGrabber.position = RotateAssembly.position + offset.difference
+    }
+
+    fun resetPositions() {
+        setAssemblyPosition(POSITIONS.DROP)
+        setAssemblyPosition(POSITIONS.COLLECTION)
+    }
+
+    fun toggleHorn() {
+        if (hornDown) {
+            hornDown = false
+            setAssemblyPosition(POSITIONS.HORNUP)
+        } else {
+            hornDown = true
+            setAssemblyPosition(POSITIONS.HORNDOWN)
+        }
+    }
+
+    fun toggleGrabber() {
+        if (grabberDown) {
+            grabberDown = false
+            setAssemblyPosition(POSITIONS.DROP)
+        } else {
+            grabberDown = true
+            setAssemblyPosition(POSITIONS.CLAMPDOWN)
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/NRobot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/NRobot.kt
@@ -1,21 +1,49 @@
 package org.firstinspires.ftc.teamcode.Robot
 
-import android.graphics.Path
+import android.os.Handler
+import android.os.Looper
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.util.ElapsedTime
 
-class NRobot constructor(val Op : OpMode) {
+class NRobot constructor(val opMode: OpMode) {
 
     companion object Modules {
         lateinit var DriveTrain : NDriveTrain
+        lateinit var Grabber : NGrabber
+        lateinit var Intake : NIntake
+        lateinit var Lift : NLift
+        lateinit var FoundationHook : NFoundationHook
+        lateinit var OSAsync : Handler
 
         fun init(Op : OpMode) {
             DriveTrain = NDriveTrain(Op)
+            Grabber = NGrabber(Op)
+            Intake = NIntake(Op)
+            Lift = NLift(Op)
+            FoundationHook = NFoundationHook(Op)
+            OSAsync = Handler(Looper.getMainLooper())
         }
     }
 
     init {
-        init(Op)
+        init(opMode)
     }
 
+    fun reset() {
+        init(opMode)
+    }
 
+    fun g2() {
+        if (opMode.gamepad2.dpad_up && opMode.gamepad2.timestamp > 50) {
+            Grabber.toggleHorn()
+        } else if (opMode.gamepad2.dpad_down && opMode.gamepad2.timestamp > 50) {
+            Grabber.toggleGrabber()
+        } else if (opMode.gamepad2.y && opMode.gamepad2.timestamp > 50) {
+            Grabber.setAssemblyPosition(NGrabber.POSITIONS.HORNDOWN)
+            OSAsync.postDelayed({
+                Grabber.setAssemblyPosition(NGrabber.POSITIONS.CLAMPDOWN)
+            }, 1000)
+        }
+        //TODO FINISH NRobot.kt
+    }
 }
