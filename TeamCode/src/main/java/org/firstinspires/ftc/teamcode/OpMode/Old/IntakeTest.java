e2492586188f416a435ca9145cd0ceeee17c9b43
idevneo <idevneo@gmail.com>
2019-12-23 15:45:23 -0600

    para slav

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Old/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Old/IntakeTest.java
@@ -9,30 +9,55 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class IntakeTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor intakeL = hardwareMap.get(DcMotor.class, "intakeL");
-        DcMotor intakeR = hardwareMap.get(DcMotor.class, "intakeR");
+//        DcMotor intakeL = hardwareMap.get(DcMotor.class, "intakeL");
+//        DcMotor intakeR = hardwareMap.get(DcMotor.class, "intakeR");
+        DcMotor liftL = hardwareMap.get(DcMotor.class, "liftL");
+        DcMotor liftR = hardwareMap.get(DcMotor.class, "liftR");
 
-          //Uncomment whichever you need to flip the intake
+        liftL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Uncomment whichever you need to flip the intake
 //        intakeL.setDirection(DcMotor.Direction.REVERSE);
 //        intakeR.setDirection(DcMotor.Direction.REVERSE);
 
+
+        //hub2
+        //port0 liftL
+        //port1 liftR
+        //port2 FR
+        //port3 BL
+
+        //hub5
 
 
         waitForStart();
 
         while (opModeIsActive()) {
-            if (gamepad1.left_bumper) {
-                //spit out
-                intakeL.setPower(-1.0);
-                intakeR.setPower(1.0);
-            } else if (gamepad1.right_bumper){
-                //intake
-                intakeL.setPower(1.0);
-                intakeR.setPower(-1.0);
+            if (gamepad2.right_stick_y > 0.05) {
+                liftL.setPower(gamepad2.left_stick_y);
+                liftR.setPower(gamepad2.left_stick_y);
+                //this is up
+            } else if (gamepad2.right_stick_y < 0.05) {
+                liftL.setPower(gamepad2.left_stick_y);
+                liftR.setPower(gamepad2.left_stick_y);
             } else {
-                intakeL.setPower(0.0);
-                intakeR.setPower(0.0);
+                liftL.setPower(0.0);
+                liftR.setPower(0.0);
             }
+//            if (gamepad1.left_bumper) {
+//                //spit out
+//                intakeL.setPower(-1.0);
+//                intakeR.setPower(1.0);
+//            } else if (gamepad1.right_bumper){
+//                //intake
+//                intakeL.setPower(1.0);
+//                intakeR.setPower(-1.0);
+//            } else {
+//                intakeL.setPower(0.0);
+//                intakeR.setPower(0.0);
+//            }
+
         }
     }
 }
